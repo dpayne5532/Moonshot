9f5ac9e3af568478f09b8be48347a8975602aa6b
Dan Payne <dpayne5532@gmail.com>
2022-01-23 10:28:03 -0700

    Prepare for day 40

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,47 +8,11 @@
 import SwiftUI
 
 
-
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
-
-
-
 struct ContentView: View {
-    
-    
-    
+ 
     var body: some View {
-        
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
-                }
-            }
-            """
-            
-            
-            let data = Data(input.utf8)
-            
-            if let user = try? JSONDecoder().decode(User.self, from: data) {
-                print(user.address.street)
-            }
-            
-            // more code to come
-        }
-        
+     
+     Text("Hello, world!")
     }
 }
 

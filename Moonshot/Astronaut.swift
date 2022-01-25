38df11831f6f902a354462a70508cb3c8c24696d
Dan Payne <dpayne5532@gmail.com>
2022-01-24 19:29:28 -0700

    Loading a specific kind of Codable data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Dan Payne on 1/24/22.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+    
+}
